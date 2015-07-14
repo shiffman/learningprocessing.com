c5b1935a406b57c5bb7a73d7f44fca79e4ee28cd
Daniel Shiffman <daniel@shiffman.net>
2015-07-14 23:17:47 +0100

    the snake example

--- javascripts/sketches/home.js
+++ javascripts/sketches/home.js
@@ -1,4 +1,6 @@
-var x = 0;
+
+// Declare an array
+var points = []; 
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
@@ -12,10 +14,24 @@ function setup() {
 
 function draw() {
   clear();
-  rect(x, 0, 10, height);
-  x += 10;
-  if (x > width) {
-    x = 0;
+  // New location
+  var point = {
+    x: mouseX,
+    y: mouseY
+  }
+  points.push(point); // Update the last spot in the array with the mouse location.
+
+  if (points.length > 50) {
+    points.splice(0,1);
+  }
+  
+  // Draw everything
+  for (var i = 0; i < points.length; i++) {
+     // Draw an ellipse for each element in the arrays. 
+     // Color and size are tied to the loop's counter: i.
+    noStroke();
+    fill(255-i*5);
+    ellipse(points[i].x,points[i].y,i,i);
   }
 }
 
