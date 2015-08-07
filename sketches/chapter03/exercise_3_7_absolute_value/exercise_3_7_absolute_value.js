8cc00f35612f28925c6f0c6536632c00bfbb6394
Daniel Shiffman <daniel@shiffman.net>
2015-08-07 13:47:54 -0400

    getting code to run in sketch-container

--- sketches/chapter03/exercise_3_7_absolute_value/exercise_3_7_absolute_value.js
+++ sketches/chapter03/exercise_3_7_absolute_value/exercise_3_7_absolute_value.js
@@ -1,10 +1,11 @@
 function setup() {
-  createCanvas(200, 200);
+  var canvas = createCanvas(200, 200);
+  canvas.parent('sketch-container');
   background(255);
 }
 
 function draw() {
   stroke(0);
-  strokeWeight(abs(pmouseX - mouseX));
+  //strokeWeight(abs(pmouseX - mouseX));
   line(pmouseX, pmouseY, mouseX, mouseY);
 }
